c5b70df180039144b6d7efea8d55f69cc74fedd6
Telmiyo <telmoberoiz@hotmail.com>
2021-04-15 09:55:01 +0200

    Npc Mover Auto

--- Game/Source/NPCMovementSystem.cpp
+++ Game/Source/NPCMovementSystem.cpp
@@ -7,4 +7,31 @@ void NPCMovementSystem::tick(ECS::World* world, float dt)
 
 		p->Translate(fPoint(npcMover->speed, 0) * dt);
 	});
+
+	world->each<NPCMoverAuto>([&](ECS::Entity* entity, ECS::ComponentHandle<NPCMoverAuto> npcMoverAuto) {
+		ECS::ComponentHandle<Position> p = entity->get<Position>();
+		//SI X ES MAYOR A 0 MOVER EN HORIZONTAL PRIMERO
+		if (npcMoverAuto->speed != 0)
+		{
+			if (npcMoverAuto->x <= 0 && npcMoverAuto->y <= 0)
+			{
+				npcMoverAuto->speed = 0;
+			}
+			else if (npcMoverAuto->x > 0)
+			{
+				p->Translate(fPoint(npcMoverAuto->speed, 0) * dt);
+				npcMoverAuto->x--;
+			}
+			else if (npcMoverAuto->y > 0)
+			{
+				p->Translate(fPoint(0, npcMoverAuto->speed) * dt);
+				npcMoverAuto->y--;
+			}
+
+		}
+		else { p->Translate(fPoint(0, 0) * dt); }
+
+		//p->Translate(fPoint(npcMover->speed, -1) * dt);
+
+		});
 }
